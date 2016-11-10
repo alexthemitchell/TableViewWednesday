6d40f660fe9c023b0754efa4742c0514fa6c2343
Alex Mitchell <alex@alexthemitchell.com>
2016-11-09 20:25:32 -0600

    Initial Commit

---
+++ DirectoryWednesday/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DirectoryWednesday
+//
+//  Created by Alex Mitchell on 11/9/16.
+//  Copyright © 2016 Grinnell AppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DirectoryWednesday/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DirectoryWednesday
+//
+//  Created by Alex Mitchell on 11/9/16.
+//  Copyright © 2016 Grinnell AppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
