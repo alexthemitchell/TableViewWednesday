184e4f4d4e87ec1113593e22a63e2288d2dd993f
Alex Mitchell <alex@alexthemitchell.com>
2016-11-23 20:57:08 -0600

    Add gcd

--- DirectoryWednesday/ViewController.m
+++ DirectoryWednesday/ViewController.m
@@ -2,26 +2,43 @@
 
 #import "Person.h"
 
+static NSString *dataURL =
+
+@"https://appdev.grinnell.edu/members.json";
+
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
 @implementation ViewController {
-  NSArray <Person *> *people;
+  NSMutableArray <Person *> *people;
 }
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
-  Person *alex = [Person new];
-  alex.name = @"alex";
-  alex.title = @"dude";
-  
-  Person *bob = [[Person alloc] init];
-  bob.name = @"bob";
-  bob.title = @"fisher";
-  
-  people = @[alex, bob];
+  people = [NSMutableArray<Person *> new];
+  [self refreshContent];
+}
+
+- (void)refreshContent {
+  NSURL *properDataURL = [NSURL URLWithString:dataURL];
+  // warn on string in task dec first
+  NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:properDataURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+    NSDictionary<NSString*, NSDictionary*> *parsedResponse = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+    NSArray<NSString *> *keys = [parsedResponse allKeys];
+    for (NSString *key in keys) {
+      NSDictionary *personDictionary = parsedResponse[key];
+      Person *person = [Person new];
+      person.name = key;
+      person.title = personDictionary[@"title"];
+      [people addObject:person];
+    }
+    dispatch_async(dispatch_get_main_queue(), ^{
+      [self.tableView reloadData];
+    });
+  }];
+  [task resume];
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
